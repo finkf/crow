a75f5f7c93e930c994930a70f43a7459c52f64b6
finkf <finkf@cis.lmu.de>
2020-01-13 16:35:39 +0100

    Wrap get_io_service into a macro to handle boost > 1.6

--- include/crow/socket_adaptors.h
+++ include/crow/socket_adaptors.h
@@ -4,6 +4,14 @@
 #include <boost/asio/ssl.hpp>
 #endif
 #include "crow/settings.h"
+
+// get_io_service() is deprecated in boost 1.7x
+#if BOOST_VERSION >= 107000
+#define CROW_GET_IO_SERVICE(s) ((boost::asio::io_context&)(s).get_executor().context())
+#else
+#define CROW_GET_IO_SERVICE(s) ((s).get_io_service())
+#endif
+
 namespace crow
 {
     using namespace boost;
@@ -19,7 +27,7 @@ namespace crow
 
 	    boost::asio::io_service& get_io_service()
         {
-            return socket_.get_io_service();
+		    return CROW_GET_IO_SERVICE(socket_);
         }
 
         tcp::socket& raw_socket()
@@ -96,7 +104,7 @@ namespace crow
 
         boost::asio::io_service& get_io_service()
         {
-            return raw_socket().get_io_service();
+		    return CROW_GET_IO_SERVICE(raw_socket());
         }
 
         template <typename F>
